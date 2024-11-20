58ee88e0803c93d64a828b3b1a9d725d93cac2f5
Natest <62326659+ncb6206@users.noreply.github.com>
2024-11-21 05:12:57 +0900

    글로벌스타일 적용 (#2)
    * Chore : 패키지 매니저 설정 수정
    
    * Design : 글로벌스타일 적용

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,82 @@
+import { css } from '@emotion/react';
+
+export const globalStyles = css`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  ul,
+  ol,
+  li {
+    list-style: none;
+  }
+
+  body {
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      'Open Sans',
+      'Helvetica Neue';
+    font-size: 16px;
+    min-width: 360px;
+    min-height: 100vh;
+    margin: 0 auto;
+    padding: 16px;
+
+    // 터치 동작 최적화
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
+
+    // 스크롤 부드럽게
+    scroll-behavior: smooth;
+    -webkit-overflow-scrolling: touch;
+
+    @media (min-width: 768px) {
+      // 태블릿
+      max-width: 768px;
+      padding: 24px;
+    }
+
+    @media (min-width: 1024px) {
+      // 데스크탑
+      max-width: 1024px;
+      padding: 32px;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  button {
+    background: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  input,
+  textarea {
+    -webkit-appearance: none;
+    appearance: none;
+  }
+`;
