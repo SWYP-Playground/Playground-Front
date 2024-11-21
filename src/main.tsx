8fb037bdaef683a48ac05b2b547c591c416f6cf4
ncb6206 <ncb6206@gmail.com>
2024-11-21 18:07:59 +0900

    Feat : 라우팅 및 MSW 설정 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,31 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
 import { Global } from '@emotion/react';
+
 import { globalStyles } from '@styles/GlobalStyle.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Global styles={globalStyles} />
-    <App />
-  </StrictMode>,
-);
+import { worker } from '@mocks/browser.ts';
+
+import AppRouter from '@router/AppRouter.tsx';
+
+const main = async () => {
+  if (import.meta.env.MODE === 'development') {
+    await worker.start({
+      serviceWorker: {
+        url: '/mockServiceWorker.js',
+      },
+      onUnhandledRequest: 'bypass',
+    });
+  }
+
+  const root = createRoot(document.querySelector('#root') as Element);
+
+  root.render(
+    <StrictMode>
+      <Global styles={globalStyles} />
+      <AppRouter />
+    </StrictMode>,
+  );
+};
+
+main();
