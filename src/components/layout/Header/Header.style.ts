7e0047de1ed29e6e7fac1074e846768d086c7cc1
ncb6206 <ncb6206@gmail.com>
2024-11-26 15:22:51 +0900

    Feat : 헤더 공통 컴포넌트 추가

---
+++ src/components/layout/Header/Header.style.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const HeaderStyling = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  width: 100%;
+  background-color: white;
+  z-index: 50;
+`;
+
+export const HeaderDiv = styled.div`
+  height: 46px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 16px;
+`;
--- src/pages/FindPlaygroundFriendPage/FindPlaygroundFriendPage.tsx
+++ src/pages/FindPlaygroundFriendPage/FindPlaygroundFriendPage.tsx
@@ -1,4 +1,7 @@
 import CustomBottomSheet from '@/components/common/BottomSheet/CustomBottomSheet';
+import Header from '@components/layout/Header/Header';
+import LeftIcon from '@assets/svg/left-icon.svg?react';
+import RightIcon from '@assets/svg/right-icon.svg?react';
 import { useBottomSheet } from '@/hooks/common/useBottomSheet';
 
 const FindPlaygroundFriendPage = () => {
@@ -6,6 +9,7 @@ const FindPlaygroundFriendPage = () => {
 
   return (
     <div>
+      <Header title="메인페이지" leftIcon={<LeftIcon />} rightIcon={<RightIcon />} />
       <button onClick={open}>바텀시트 열기</button>
       <CustomBottomSheet isOpen={isOpen} onClose={close}>
         <h2>Bottom Sheet Content</h2>
