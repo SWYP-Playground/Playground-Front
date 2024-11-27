6f642b7499b7d5cd8c420275680bf7f53860c320
Natest <62326659+ncb6206@users.noreply.github.com>
2024-11-28 03:34:11 +0900

    Remove : PlayGround 중복 폴더 삭제

--- src/components/PlayGround/PlayGroundButton/PlayGroundButton.tsx
+++
@@ -1,7 +0,0 @@
-import { PlayGroundButtonStyling } from '@/components/playGround/PlayGroundButton/PlayGroundButton.style';
-
-const PlayGroundButton = () => {
-  return <PlayGroundButtonStyling>모집글 등록하기</PlayGroundButtonStyling>;
-};
-
-export default PlayGroundButton;
