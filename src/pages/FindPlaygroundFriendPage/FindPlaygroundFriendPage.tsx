9de6aa20f896104ccda35075867b56dddfa982da
ncb6206 <ncb6206@gmail.com>
2024-11-27 14:38:19 +0900

    Feat : 놀이터 모임 모집 페이지 작업중

--- src/pages/FindPlaygroundFriendPage/FindPlaygroundFriendPage.tsx
+++ src/pages/FindPlaygroundFriendPage/FindPlaygroundFriendPage.tsx
@@ -1,21 +1,33 @@
+import { Flex } from '@radix-ui/themes';
+import { useNavigate } from 'react-router-dom';
+
 import CustomBottomSheet from '@/components/common/BottomSheet/CustomBottomSheet';
 import Header from '@components/layout/Header/Header';
-import LeftIcon from '@assets/svg/left-icon.svg?react';
-import RightIcon from '@assets/svg/right-icon.svg?react';
+import PlayGroundButton from '@/components/PlayGround/PlayGroundButton/PlayGroundButton';
+import PlayGroundSearchBar from '@/components/PlayGround/PlayGroundSearchBar/PlayGroundSearchBar';
 import { useBottomSheet } from '@/hooks/common/useBottomSheet';
+import LeftIcon from '@assets/svg/left-icon.svg?react';
 
 const FindPlaygroundFriendPage = () => {
+  const navigate = useNavigate();
+
+  const goToBackPage = () => {
+    navigate(-1);
+  };
+
   const { isOpen, open, close } = useBottomSheet();
 
   return (
-    <div>
-      <Header title="메인페이지" leftIcon={<LeftIcon />} rightIcon={<RightIcon />} />
+    <Flex direction="column">
+      <Header title="놀이터 찾기" leftIcon={<LeftIcon />} onLeftClick={goToBackPage} />
+      <PlayGroundSearchBar />
       <button onClick={open}>바텀시트 열기</button>
       <CustomBottomSheet isOpen={isOpen} onClose={close}>
         <h2>Bottom Sheet Content</h2>
         <p>여기에 원하는 컨텐츠를 넣을 수 있습니다.</p>
       </CustomBottomSheet>
-    </div>
+      <PlayGroundButton />
+    </Flex>
   );
 };
 
