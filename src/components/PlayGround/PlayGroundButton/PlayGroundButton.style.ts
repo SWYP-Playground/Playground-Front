e48259aa3cbcd051f409593f4eab686b4ee530b4
ncb6206 <ncb6206@gmail.com>
2024-11-27 14:37:27 +0900

    Feat : 모집글 등록 버튼

---
+++ src/components/PlayGround/PlayGroundButton/PlayGroundButton.style.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+import { Button } from '@radix-ui/themes';
+
+export const PlayGroundButtonStyling = styled(Button)`
+  position: fixed;
+  display: flex;
+  bottom: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  padding: 25px;
+  width: calc(100% - 40px);
+  max-width: 500px;
+  font-size: 16px;
+  z-index: 1000;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  color: ${(props) => props.theme.colors.black0};
+  background-color: ${(props) => props.theme.colors.black900};
+`;
---
+++ src/components/PlayGround/PlayGroundButton/PlayGroundButton.tsx
@@ -0,0 +1,7 @@
+import { PlayGroundButtonStyling } from '@components/PlayGround/PlayGroundButton/PlayGroundButton.style';
+
+const PlayGroundButton = () => {
+  return <PlayGroundButtonStyling>모집글 등록하기</PlayGroundButtonStyling>;
+};
+
+export default PlayGroundButton;
