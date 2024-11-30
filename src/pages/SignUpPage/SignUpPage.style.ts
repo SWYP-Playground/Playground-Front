74444bc76b6468c4313d81c870c52103ea58e0c2
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-11-30 17:45:03 +0900

    Refactor : react-hook-form으로 리팩토링 (회원가입 페이지)

---
+++ src/pages/SignUpPage/SignUpPage.style.ts
@@ -0,0 +1,190 @@
+import styled from '@emotion/styled';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.black0};
+`;
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  padding: 8px 22px;
+  margin-bottom: 30px;
+`;
+
+export const LeftIcon = styled.img`
+  width: 8px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const Title = styled.p`
+  font-size: 18px;
+  font-weight: 900;
+  color: ${(props) => props.theme.colors.black900};
+  margin-left: 40px;
+`;
+
+export const ToLogin = styled(Link)`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${(props) => props.theme.colors.black900};
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 16px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Label = styled.label`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${(props) => props.theme.colors.black800};
+`;
+
+export const Input = styled.input<{ hasError: boolean }>`
+  width: 100%;
+  height: 44px;
+  padding: 12px 16px;
+  border: 1px solid ${(props) => (props.hasError ? props.theme.colors.red : 'transparent')};
+  border-radius: 5px;
+
+  color: ${(props) => props.theme.colors.black800};
+  background-color: ${(props) => props.theme.colors.black100};
+
+  font-size: 14px;
+  margin-top: 10px;
+
+  &:focus {
+    border: 2px solid ${(props) => (props.hasError ? props.theme.colors.red : 'transparent')};
+    outline: none;
+  }
+`;
+
+export const EmailContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const DuplicateCheckButton = styled.button`
+  height: 44px;
+  padding: 0 16px;
+  font-weight: 600;
+  white-space: nowrap;
+  background-color: ${(props) => props.theme.colors.black800};
+  color: ${(props) => props.theme.colors.black0};
+  margin-top: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:disabled {
+    background-color: ${(props) => props.theme.colors.black400};
+    color: ${(props) => props.theme.colors.black600};
+
+    cursor: not-allowed;
+  }
+`;
+
+export const CheckboxWrapper = styled(Checkbox.Root)`
+  width: 20px;
+  height: 20px;
+  border: 1px solid ${(props) => props.theme.colors.black800};
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CheckboxIndicator = styled(Checkbox.Indicator)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.colors.black800};
+  color: white;
+  width: 100%;
+  height: 100%;
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+export const CheckboxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-top: 40px;
+`;
+
+export const CheckboxLabel = styled.label`
+  font-size: 14px;
+`;
+
+export const DetailsLink = styled.a`
+  font-size: 12px;
+  font-weight: 600;
+  padding: 2px 8px;
+  color: ${(props) => props.theme.colors.black600};
+  border: 1px solid ${(props) => props.theme.colors.black300};
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+export const SubmitButton = styled.button`
+  width: calc(100% - 32px);
+  height: 50px;
+  max-width: 400px;
+
+  position: fixed;
+  bottom: 16px;
+  left: 50%;
+  transform: translateX(-50%);
+
+  background-color: ${({ disabled, theme }) =>
+    disabled ? theme.colors.black400 : theme.colors.primary1};
+  color: ${({ disabled, theme }) => (disabled ? theme.colors.black600 : theme.colors.black900)};
+  border: none;
+  border-radius: 8px;
+
+  font-size: 16px;
+  font-weight: 600;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  transition: opacity 0.2s;
+`;
+
+export const ErrorMessage = styled.span`
+  font-size: 12px;
+  color: ${(props) => props.theme.colors.red};
+  margin-top: 5px;
+`;
+
+export const HintMessage = styled.span`
+  font-size: 12px;
+  font-weight: 500;
+  color: ${(props) => props.theme.colors.black500};
+  margin-top: 5px;
+`;
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,123 +1,65 @@
-import styled from '@emotion/styled';
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { useForm } from 'react-hook-form';
 import { CheckIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Container,
+  HeaderContainer,
+  LeftIcon,
+  Title,
+  ToLogin,
+  Form,
+  InputContainer,
+  Label,
+  Input,
+  EmailContainer,
+  DuplicateCheckButton,
+  CheckboxWrapper,
+  CheckboxIndicator,
+  CheckboxContainer,
+  CheckboxLabel,
+  DetailsLink,
+  SubmitButton,
+  ErrorMessage,
+  HintMessage,
+} from './SignUpPage.style.ts';
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  agreement: boolean;
+}
 
 const SignUpPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const [nameWarning, setNameWaring] = useState('');
-  const [emailWarning, setEmailWarning] = useState('');
-  const [passwordWarning, setPasswordWarning] = useState('');
-  const [confirmPasswordWarning, setConfirmPasswordWarning] = useState('');
-
-  const [isSubmitClicked, setIsSubmitClicked] = useState<boolean>(false);
-  const [isChecked, setIsChecked] = useState(false);
-
   const navigate = useNavigate();
 
-  // 이메일 유효성 검사
-  const validateEmail = (email: string): void => {
-    if (!email) {
-      setEmailWarning('');
-      return;
-    }
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      setEmailWarning('올바른 이메일 형식이 아닙니다.');
-    } else {
-      setEmailWarning('');
-    }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm<FormData>({
+    mode: 'onChange',
+  });
+
+  const [isEmailChecked, setIsEmailChecked] = useState(false);
+  const [isAgreeChecked, setIsAgreeChecked] = useState(false);
+  const password = watch('password');
+
+  const onSubmit = (data: FormData) => {
+    console.log('회원가입 데이터:', data);
   };
 
-  // 비밀번호 유효성 검사
-  const validatePassword = (password: string): void => {
-    if (!password) {
-      setPasswordWarning('');
-      return;
-    }
-    const lengthValid = password.length >= 6 && password.length <= 20;
-    const combinationValid =
-      /[A-Z]/.test(password) &&
-      /[a-z]/.test(password) &&
-      /[0-9]/.test(password) &&
-      /[!@#$%^&*(),.?":{}|<>]/.test(password);
-
-    if (!lengthValid) {
-      setPasswordWarning('비밀번호는 최소 6자리에서 최대 20자리여야 합니다.');
-    } else if (!combinationValid) {
-      setPasswordWarning('비밀번호는 영어 대소문자, 숫자, 특수문자를 모두 포함해야 합니다.');
-    } else {
-      setPasswordWarning('');
-    }
+  const checkEmailDuplicate = () => {
+    console.log('이메일 중복 확인');
+    setIsEmailChecked(true);
   };
 
-  // 비밀번호 재입력 유효성 검사
-  const validateConfirmPassword = (confirmPassword: string): void => {
-    if (!confirmPassword) {
-      setConfirmPasswordWarning('');
-      return;
-    }
-    if (confirmPassword !== password) {
-      setConfirmPasswordWarning('비밀번호가 일치하지 않습니다.');
-    } else {
-      setConfirmPasswordWarning('');
-    }
-  };
-
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    setName(inputValue);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    setEmail(inputValue);
-    validateEmail(inputValue);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    setPassword(inputValue);
-    validatePassword(inputValue);
-    validateConfirmPassword(confirmPassword);
-  };
-
-  const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    setConfirmPassword(inputValue);
-    validateConfirmPassword(inputValue);
-  };
-
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
-
-  const handleSubmitClick = (): void => {
-    if (
-      !name ||
-      !!nameWarning ||
-      !email ||
-      !!emailWarning ||
-      !password ||
-      !!passwordWarning ||
-      !confirmPassword ||
-      !!confirmPasswordWarning ||
-      !isChecked
-    ) {
-      console.log('입력 조건을 모두 충족해야 합니다.');
-      return;
-    }
-
-    setIsSubmitClicked(true);
-    setTimeout(() => {
-      setIsSubmitClicked(false);
-      navigate('/');
-    }, 1000);
+  const checkAgreeButton = () => {
+    console.log('동의 버튼 체크 확인');
+    setIsAgreeChecked(true);
   };
 
   return (
@@ -125,274 +67,114 @@ const SignUpPage = () => {
       <HeaderContainer>
         <LeftIcon src="/src/assets/svg/left-icon.svg" alt="LeftIcon" onClick={() => navigate(-1)} />
         <Title>회원가입</Title>
-        <ToHome to="/">로그인</ToHome>
+        <ToLogin to="/sign-in">로그인</ToLogin>
       </HeaderContainer>
-
-      <Form>
-        <Label>
-          이름
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputContainer>
+          <Label htmlFor="name">이름</Label>
           <Input
-            type="text"
+            id="name"
             placeholder="이름을 입력해 주세요"
-            value={name}
-            onChange={handleNameChange}
-            hasError={!!nameWarning}
+            {...register('name', { required: '이름을 입력해 주세요.' })}
+            hasError={!!errors.name}
           />
-          <WarningMessageContainer>
-            {nameWarning && <WarningMessage>{nameWarning}</WarningMessage>}
-          </WarningMessageContainer>
-        </Label>
-        <Label>
-          이메일
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+        </InputContainer>
+
+        <InputContainer>
+          <Label htmlFor="email">이메일</Label>
+          <EmailContainer>
+            <Input
+              id="email"
+              placeholder="이메일을 입력해 주세요"
+              {...register('email', {
+                required: '이메일을 입력해 주세요.',
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: '올바른 이메일 형식이 아닙니다.',
+                },
+              })}
+              hasError={!!errors.email}
+            />
+            <DuplicateCheckButton
+              type="button"
+              onClick={checkEmailDuplicate}
+              disabled={isEmailChecked}
+            >
+              {isEmailChecked ? '확인 완료' : '중복 확인'}
+            </DuplicateCheckButton>
+          </EmailContainer>
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </InputContainer>
+
+        <InputContainer>
+          <Label htmlFor="password">비밀번호</Label>
           <Input
-            type="email"
-            placeholder="이메일을 입력해 주세요"
-            value={email}
-            onChange={handleEmailChange}
-            hasError={!!emailWarning}
-          />
-          <WarningMessageContainer>
-            {emailWarning && <WarningMessage>{emailWarning}</WarningMessage>}
-          </WarningMessageContainer>
-        </Label>
-        <Label>
-          비밀번호
-          <Input
+            id="password"
             type="password"
             placeholder="비밀번호를 입력해 주세요"
-            value={password}
-            onChange={handlePasswordChange}
-            hasError={!!passwordWarning}
+            {...register('password', {
+              required: '비밀번호를 입력해 주세요.',
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{6,20}$/,
+                message: '6-20자 이내 *영문 대소문자, 숫자, 특수문자 필수',
+              },
+            })}
+            hasError={!!errors.password}
           />
-          <WarningMessageContainer>
-            {passwordWarning ? (
-              <WarningMessage>{passwordWarning}</WarningMessage>
-            ) : (
-              <HintMessage>6~20자 이내 *영문 대소문자, 숫자, 특수문자 필수</HintMessage>
-            )}
-          </WarningMessageContainer>
-        </Label>
-        <Label>
-          비밀번호 재입력
+          {errors.password ? (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          ) : (
+            <HintMessage>6-20자 이내 *영문 대소문자, 숫자, 특수문자 필수</HintMessage>
+          )}
+        </InputContainer>
+
+        <InputContainer>
+          <Label htmlFor="confirmPassword">비밀번호 재입력</Label>
           <Input
+            id="confirmPassword"
             type="password"
-            placeholder="비밀번호를 한번 더 입력해 주세요"
-            value={confirmPassword}
-            onChange={handleConfirmPasswordChange}
-            hasError={!!confirmPasswordWarning}
+            placeholder="비밀번호를 다시 입력해 주세요"
+            {...register('confirmPassword', {
+              required: '비밀번호를 재입력해 주세요.',
+              validate: (value) => value === password || '비밀번호가 일치하지 않습니다.',
+            })}
+            hasError={!!errors.confirmPassword}
           />
-          <WarningMessageContainer>
-            {confirmPasswordWarning ? (
-              <WarningMessage>{confirmPasswordWarning}</WarningMessage>
-            ) : (
-              <HintMessage>6~20자 이내 *영문 대소문자, 숫자, 특수문자 필수</HintMessage>
-            )}
-          </WarningMessageContainer>
-        </Label>
+          {errors.confirmPassword ? (
+            <ErrorMessage>{errors.confirmPassword.message}</ErrorMessage>
+          ) : (
+            <HintMessage>6-20자 이내 *영문 대소문자, 숫자, 특수문자 필수</HintMessage>
+          )}
+        </InputContainer>
+
         <CheckboxContainer>
-          <RadixCheckbox
-            id="terms"
-            checked={isChecked}
-            onCheckedChange={(checked) => setIsChecked(checked === true)}
+          <CheckboxWrapper
+            onClick={checkAgreeButton}
+            disabled={isAgreeChecked}
+            id="agreement"
+            {...register('agreement', {
+              required: '개인정보약관에 동의해야 합니다.',
+            })}
           >
             <CheckboxIndicator>
               <CheckIcon />
             </CheckboxIndicator>
-          </RadixCheckbox>
-          <CheckboxLabel htmlFor="terms">개인정보 약관에 동의합니다</CheckboxLabel>
-          <DetailsLink href="/privacy-policy" target="_blank">
-            자세히 보기
-          </DetailsLink>
+          </CheckboxWrapper>
+          <CheckboxLabel htmlFor="agreement">개인정보약관에 동의합니다</CheckboxLabel>
+          {errors.agreement && <ErrorMessage>{errors.agreement.message}</ErrorMessage>}
+          <DetailsLink>자세히 보기</DetailsLink>
         </CheckboxContainer>
-        <ButtonContainer>
-          <SubmitButton
-            type="button"
-            isClicked={isSubmitClicked}
-            onClick={handleSubmitClick}
-            disabled={
-              !name ||
-              !!nameWarning ||
-              !email ||
-              !!emailWarning ||
-              !password ||
-              !!passwordWarning ||
-              !confirmPassword ||
-              !!confirmPasswordWarning ||
-              !isChecked
-            }
-          >
-            완료
-          </SubmitButton>
-        </ButtonContainer>
+
+        <SubmitButton
+          type="submit"
+          disabled={!isValid || isSubmitting || !isEmailChecked || !isAgreeChecked}
+        >
+          완료
+        </SubmitButton>
       </Form>
     </Container>
   );
 };
 
 export default SignUpPage;
-
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: ${(props) => props.theme.colors.black0};
-`;
-
-const HeaderContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  padding: 8px 22px;
-  margin-bottom: 30px;
-`;
-
-const LeftIcon = styled.img`
-  width: 8px;
-  position: flex;
-  justify-content: flex-start;
-
-  cursor: pointer;
-
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-const Title = styled.p`
-  justify-content: center;
-  font-size: 18px;
-  font-weight: 900;
-  color: ${(props) => props.theme.colors.black900};
-  margin-left: 20px;
-`;
-
-const ToHome = styled(Link)`
-  font-weight: 400;
-  color: ${(props) => props.theme.colors.black900};
-`;
-
-const Form = styled.form`
-  width: 100%;
-  max-width: 320px;
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  flex-grow: 1;
-`;
-
-const Label = styled.label`
-  font-size: 14px;
-  font-weight: 500;
-  color: ${(props) => props.theme.colors.black800};
-`;
-
-const Input = styled.input<{ hasError: boolean }>`
-  width: 100%;
-  height: 44px;
-  padding: 12px 16px;
-  border: 1px solid ${(props) => (props.hasError ? props.theme.colors.red : 'transparent')};
-  border-radius: 8px;
-
-  color: ${(props) => props.theme.colors.black800};
-  background-color: ${(props) => props.theme.colors.black100};
-
-  font-size: 14px;
-  margin-top: 10px;
-
-  &:focus {
-    border: 2px solid ${(props) => (props.hasError ? props.theme.colors.red : 'transparent')};
-    outline: none;
-  }
-`;
-
-const HintMessage = styled.p`
-  font-size: 12px;
-  color: ${(props) => props.theme.colors.black500};
-`;
-
-const WarningMessageContainer = styled.div`
-  min-height: 20px;
-  margin-top: 5px;
-`;
-
-const WarningMessage = styled.p`
-  font-size: 12px;
-  color: ${(props) => props.theme.colors.red};
-`;
-const ButtonContainer = styled.div`
-  width: 100%;
-  margin-top: auto;
-`;
-const CheckboxContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  flex-grow: 1;
-`;
-
-const CheckboxLabel = styled.label`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  font-size: 14px;
-  color: ${(props) => props.theme.colors.black800};
-  font-weight: 500;
-  cursor: pointer;
-`;
-
-const RadixCheckbox = styled(CheckboxPrimitive.Root)`
-  all: unset;
-  width: 20px;
-  height: 20px;
-  border: 1px solid ${(props) => props.theme.colors.black800};
-  border-radius: 4px;
-  background-color: ${(props) => (props.checked ? props.theme.colors.black800 : 'white')};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  align-items: center;
-`;
-
-const CheckboxIndicator = styled(CheckboxPrimitive.Indicator)`
-  color: white;
-  font-size: 20px;
-`;
-
-const DetailsLink = styled.a`
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 600;
-  color: ${(props) => props.theme.colors.black600};
-  text-decoration: none;
-  padding: 2px 8px;
-  border-radius: 4px;
-  border: 1px solid ${(props) => props.theme.colors.black300};
-`;
-interface SubmitButtonProps {
-  isClicked: boolean;
-}
-
-const SubmitButton = styled.button<SubmitButtonProps>`
-  width: 100%;
-  height: 50px;
-  padding: 16px 0;
-  background-color: ${(props) =>
-    props.disabled ? props.theme.colors.black200 : props.theme.colors.primary1};
-  color: ${(props) => (props.disabled ? props.theme.colors.black400 : props.theme.colors.black900)};
-  border: none;
-  border-radius: 8px;
-  font-size: 16px;
-  font-weight: 600;
-  margin-top: 10px;
-  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
-
-  &:hover {
-    opacity: ${(props) => (props.disabled ? '1' : '0.8')};
-  }
-`;
