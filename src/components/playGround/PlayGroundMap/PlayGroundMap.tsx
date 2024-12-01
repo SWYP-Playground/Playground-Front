3cb4a438b16cf8f122c8b10ad9e53a742d493675
Natest <62326659+ncb6206@users.noreply.github.com>
2024-12-01 22:35:39 +0900

    카카오 지도 관련 컴포넌트 설정 (#16)
    * Feat : 모집글 등록 버튼
    
    * Feat : 놀이터 검색 창 작업중
    
    * Feat : 놀이터 모임 모집 페이지 작업중
    
    * Refactor : 카카오 맵 로더를 hook으로 분리
    
    * Rename : PlayGround 폴더를 playGround로 폴더명 변경
    
    * Feat : 카카오 맵 컴포넌트 설정
    
    * Feat : GeoLocation 훅 적용
    
    * Remove : PlayGround 중복 폴더 삭제
    
    * Docs : 아이콘 svg 파일 추가
    
    * Feat : 놀이터 찾기 페이지 추가
    
    * Refactor : path 상수 value 값 변경
    
    * Feat : 지도 관련 설정
    
    * Refactor : 바텀시트 백드롭 추가
    
    * Refactor : 모집글 등록하기 버튼 네비게이션 설정
    
    * Refactor : 놀이터 검색창 설정
    
    * Refactor : 모집글 등록 페이지 수정
    
    * Feat : 디바운스 hook 추가

---
+++ src/components/playGround/PlayGroundMap/PlayGroundMap.tsx
@@ -0,0 +1,94 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+import useKakaoLoader from '@/hooks/common/useKakaoLoader';
+import { MAP_DEFAULT_CENTER, MAP_INITIAL_ZOOM_LEVEL } from '@/constants/map';
+import useLocation from '@/hooks/common/useLocation';
+import CrossHair from '@/assets/svg/crosshair.svg?react';
+import {
+  CurrentPositionButton,
+  PlayGroundMapDiv,
+} from '@/components/playGround/PlayGroundMap/PlayGroundMap.style';
+
+export interface CenterType {
+  lat: number;
+  lng: number;
+}
+
+const PlayGroundMap = () => {
+  useKakaoLoader();
+  const [center, setCenter] = useState<CenterType>(MAP_DEFAULT_CENTER);
+  const mapRef = useRef<kakao.maps.Map>(null);
+
+  const { coords, error: locationError } = useLocation({
+    enableHighAccuracy: true,
+    timeout: 10000,
+    maximumAge: 0,
+  });
+
+  console.log(center, coords);
+
+  useEffect(() => {
+    if (coords.latitude && coords.longitude) {
+      setCenter({
+        lat: coords.latitude,
+        lng: coords.longitude,
+      });
+    }
+  }, [coords.latitude, coords.longitude]);
+
+  useEffect(() => {
+    if (coords.latitude && coords.longitude) {
+      setCenter({ lat: coords.latitude, lng: coords.longitude });
+    }
+  }, [coords.latitude, coords.longitude]);
+
+  const moveCenter = useCallback(() => {
+    console.log(coords.latitude, coords.longitude);
+    if (!mapRef.current || !coords.latitude || !coords.longitude) return;
+
+    const newCenter = new kakao.maps.LatLng(coords.latitude, coords.longitude);
+    mapRef.current.setCenter(newCenter);
+  }, [coords.latitude, coords.longitude]);
+
+  console.log(coords, locationError);
+
+  return (
+    <PlayGroundMapDiv>
+      <Map
+        center={center}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        level={MAP_INITIAL_ZOOM_LEVEL}
+        zoomable={true}
+        draggable={true}
+        ref={mapRef}
+      >
+        <MapMarker
+          position={center}
+          title="현재 위치"
+          image={{
+            src: 'src/assets/svg/player-marker.svg',
+            size: {
+              width: 100,
+              height: 100,
+            },
+            options: {
+              offset: {
+                x: 45,
+                y: 45,
+              },
+            },
+          }}
+        />
+        <CurrentPositionButton title="현재 위치로 이동" onClick={moveCenter}>
+          <CrossHair />
+        </CurrentPositionButton>
+      </Map>
+    </PlayGroundMapDiv>
+  );
+};
+
+export default PlayGroundMap;
