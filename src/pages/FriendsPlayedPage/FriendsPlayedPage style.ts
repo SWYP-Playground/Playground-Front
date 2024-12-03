84b419c1e5c648d8a799f69019d332aeda266dad
ncb6206 <ncb6206@gmail.com>
2024-12-04 00:06:23 +0900

    Refactor : 나랑 논 친구 페이지 수정

---
+++ src/pages/FriendsPlayedPage/FriendsPlayedPage style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { Flex } from '@radix-ui/themes';
+
+export const FriendsPlayedFlex = styled(Flex)`
+  height: calc(100dvh - 106px);
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+`;
