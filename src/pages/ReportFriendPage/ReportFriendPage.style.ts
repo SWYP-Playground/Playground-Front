18dfbe731fbe795bf62e97dea60c33bd9d1ffd8c
ncb6206 <ncb6206@gmail.com>
2024-12-05 03:10:22 +0900

    Refactor : 기존 신고 페이지에 있던 TextArea 분리

--- src/pages/ReportFriendPage/ReportFriendPage.style.ts
+++ src/pages/ReportFriendPage/ReportFriendPage.style.ts
@@ -1,53 +1,7 @@
 import styled from '@emotion/styled';
-import { Button, Flex } from '@radix-ui/themes';
+import { Flex } from '@radix-ui/themes';
 
 export const ReportFriendFlex = styled(Flex)`
   height: calc(100dvh - 106px);
   flex: 1;
-  flex-direction: column;
-  align-items: center;
-  gap: 5px;
-`;
-
-export const ReportTitleSpan = styled.span`
-  width: 90%;
-  align-self: flex-start;
-`;
-
-export const ReportTextFlex = styled(Flex)`
-  width: 90%;
-  padding: 16px;
-  flex-direction: column;
-  background-color: ${({ theme }) => theme.colors.black200};
-  border-radius: 6px;
-`;
-
-export const ReportTextArea = styled.textarea`
-  display: block;
-  width: 100%;
-  height: 150px;
-  color: ${({ theme }) => theme.colors.black900};
-  background-color: ${({ theme }) => theme.colors.black200};
-  resize: none;
-  border: none;
-  outline: none;
-  flex-grow: 1;
-  cursor: auto;
-  font-size: 14px;
-  line-height: 20px;
-`;
-
-export const LengthSpan = styled.span`
-  align-self: flex-end;
-  color: ${({ theme }) => theme.colors.black700};
-`;
-
-export const ReportButton = styled(Button)`
-  width: 90%;
-  margin-top: auto;
-  margin-bottom: 5px;
-  padding: 20px 0;
-  background-color: ${({ disabled, theme }) =>
-    disabled ? theme.colors.black400 : theme.colors.primary1};
-  color: ${({ disabled, theme }) => (disabled ? theme.colors.black600 : theme.colors.black900)};
 `;
--- src/pages/ReportFriendPage/ReportFriendPage.tsx
+++ src/pages/ReportFriendPage/ReportFriendPage.tsx
@@ -1,30 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 
 import Header from '@/components/layout/Header/Header';
-import {
-  LengthSpan,
-  ReportButton,
-  ReportFriendFlex,
-  ReportTextArea,
-  ReportTextFlex,
-  ReportTitleSpan,
-} from '@/pages/ReportFriendPage/ReportFriendPage.style';
+import { ReportFriendFlex } from '@/pages/ReportFriendPage/ReportFriendPage.style';
 import LeftIcon from '@/assets/svg/left-icon.svg?react';
-import { ChangeEvent, useState } from 'react';
-import { TEXT_MAX_LENGTH } from '@/constants/text';
-import { Flex } from '@radix-ui/themes';
+import TextInput from '@/components/common/TextInput/TextInput';
 
 const ReportFriendPage = () => {
   const navigate = useNavigate();
-  const [text, setText] = useState<string>('');
-
-  const onChangeText = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    const inputValue = event.target.value;
-
-    if (inputValue.length <= TEXT_MAX_LENGTH) {
-      setText(inputValue);
-    }
-  };
 
   const goBackToPage = () => {
     navigate(-1);
@@ -33,20 +15,10 @@ const ReportFriendPage = () => {
   return (
     <ReportFriendFlex>
       <Header title="신고하기" leftIcon={<LeftIcon />} onLeftClick={goBackToPage} />
-      <Flex width="90%">
-        <ReportTitleSpan>내용</ReportTitleSpan>
-      </Flex>
-      <ReportTextFlex>
-        <ReportTextArea
-          placeholder="친구를 신고하는 이유와 자세한 상황을 설명해 주세요."
-          value={text}
-          onChange={onChangeText}
-        />
-        <LengthSpan>
-          {text.length}/{TEXT_MAX_LENGTH}자
-        </LengthSpan>
-      </ReportTextFlex>
-      <ReportButton disabled={!text}>완료</ReportButton>
+      <TextInput
+        placeholder="친구를 신고하는 이유와 자세한 상황을 설명해 주세요."
+        buttonLabel="완료"
+      />
     </ReportFriendFlex>
   );
 };
