97d159e7f3a823ddab2daa20b1b018d724d08a9e
ncb6206 <ncb6206@gmail.com>
2024-12-10 04:27:16 +0900

    Refactor : 놀이터 검색 로직 수정

--- src/pages/FindPlaygroundFriendPage/FindPlaygroundFriendPage.tsx
+++ src/pages/FindPlaygroundFriendPage/FindPlaygroundFriendPage.tsx
@@ -1,16 +1,39 @@
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Flex } from '@radix-ui/themes';
 
 import CustomBottomSheet from '@/components/common/BottomSheet/CustomBottomSheet';
 import Header from '@components/layout/Header/Header';
 import PlayGroundButton from '@/components/playGround/PlayGroundButton/PlayGroundButton';
 import PlayGroundSearchBar from '@/components/playGround/PlayGroundSearchBar/PlayGroundSearchBar';
 import { useBottomSheet } from '@/hooks/common/useBottomSheet';
-import LeftIcon from '@assets/svg/left-icon.svg?react';
 import PlayGroundMap from '@/components/playGround/PlayGroundMap/PlayGroundMap';
 import { PlaygroundFlex } from '@/pages/FindPlaygroundFriendPage/FindPlaygroundFriendPage.style';
+import PlayGroundItem from '@/components/playGround/PlayGroundItem/PlayGroundItem';
+import LeftIcon from '@assets/svg/left-icon.svg?react';
+
+const playgroundData = [
+  {
+    name: '서리풀 상상나라 숲속학교 놀이터',
+    address: '서울 서초구 서초대로 160-7',
+    distance: '1.1',
+  },
+  {
+    name: '도구머리 숲 놀이터',
+    address: '서울 서초구 강남대로 221 5층 공원녹지과',
+    distance: '1.1',
+  },
+  {
+    name: '서초그랑자이 기부채납공원 내 놀이터',
+    address: '서울 서초구 효령로 391',
+    distance: '1.1',
+  },
+];
 
 const FindPlaygroundFriendPage = () => {
   const navigate = useNavigate();
+  const [searchParams, _] = useSearchParams();
+  const query = searchParams.get('query');
 
   const goToBackPage = () => {
     navigate(-1);
@@ -18,14 +41,22 @@ const FindPlaygroundFriendPage = () => {
 
   const { isOpen, open, close } = useBottomSheet();
 
+  useEffect(() => {
+    if (query) {
+      open();
+    }
+  }, [query]);
+
   return (
     <PlaygroundFlex direction="column">
       <Header title="놀이터 찾기" leftIcon={<LeftIcon />} onLeftClick={goToBackPage} />
-      <PlayGroundSearchBar />
-      <button onClick={open}>바텀시트 열기</button>
-      <CustomBottomSheet isOpen={isOpen} onClose={close}>
-        <h2>Bottom Sheet Content</h2>
-        <p>여기에 원하는 컨텐츠를 넣을 수 있습니다.</p>
+      <Flex style={{ padding: '0 16px' }}>
+        <PlayGroundSearchBar />
+      </Flex>
+      <CustomBottomSheet isOpen={isOpen} onClose={close} showBackdrop={false}>
+        {playgroundData.map((item) => (
+          <PlayGroundItem name={item.name} address={item.address} distance={item.distance} />
+        ))}
       </CustomBottomSheet>
       <PlayGroundMap />
       <PlayGroundButton />
--- src/pages/PlaygroundSearchPage/PlaygroundSearchPage.style.tsx
+++ src/pages/PlaygroundSearchPage/PlaygroundSearchPage.style.tsx
@@ -1,16 +1,8 @@
 import styled from '@emotion/styled';
-import { Button } from '@radix-ui/themes';
+import { Flex } from '@radix-ui/themes';
 
-export const CreateRoomButton = styled(Button)`
-  background-color: ${(props) => props.theme.colors.primary1};
-  display: 'inline-flex';
-  width: '100%';
-  height: '100%';
-  padding: 6px 10px;
-  border-radius: 999px;
-  justify-content: 'center';
-  align-items: 'center';
-  gap: 4;
-  color: ${(props) => props.theme.colors.black900};
-  cursor: pointer;
+export const PlayGroundItemFlex = styled(Flex)`
+  flex-direction: column;
+  width: calc(100% - 32px);
+  align-self: center;
 `;
