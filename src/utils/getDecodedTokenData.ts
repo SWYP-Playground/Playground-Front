219a399baf3771d191f056e70329851246a49664
ncb6206 <ncb6206@gmail.com>
2024-12-16 03:59:30 +0900

    Feat : JWT decode 함수 추가

---
+++ src/utils/getDecodedTokenData.ts
@@ -0,0 +1,36 @@
+import { ACCESS_TOKEN_KEY } from '@/constants/api';
+import { jwtDecode } from 'jwt-decode';
+
+interface DecodedToken {
+  sub: string;
+  nickname: string;
+  parentId: string;
+  exp: number;
+  iat: number;
+}
+
+const getDecodedTokenData = () => {
+  try {
+    const authStorage = localStorage.getItem(ACCESS_TOKEN_KEY);
+
+    if (!authStorage) {
+      throw new Error('Auth storage not found');
+    }
+
+    const { state } = JSON.parse(authStorage);
+    const token = state?.token;
+
+    if (!token) {
+      throw new Error('Token not found in auth storage');
+    }
+
+    const decodedToken = jwtDecode<DecodedToken>(token);
+    const { sub, nickname, parentId } = decodedToken;
+    return { sub, nickname, parentId };
+  } catch (error) {
+    console.error('Failed to decode token:', error);
+    throw new Error('Failed to decode token');
+  }
+};
+
+export default getDecodedTokenData;
