1fd8f3529a428206c105f4bb2da121edf944e1b0
ncb6206 <ncb6206@gmail.com>
2024-12-16 04:14:16 +0900

    Feat : 모집글 목록 리스트 페이지 추가

---
+++ src/pages/PlaygroundRoomListPage/PlaygroundRoomListPage.style.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { Button, Flex } from '@radix-ui/themes';
+
+export const PlayGroundRoomListFlex = styled(Flex)`
+  flex-direction: column;
+  height: calc(100dvh - 106px);
+  flex: 1;
+`;
+
+export const CreatePlayGroundRoomButton = styled(Button)`
+  margin-top: auto;
+  width: 90%;
+  padding: 30px 0;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 8px;
+  margin-bottom: 16px;
+  color: ${({ theme }) => theme.colors.black900};
+  background-color: ${({ theme }) => theme.colors.primary1};
+  cursor: pointer;
+`;
