2490ca2bae5ceaaabbf2bc8ef5023b7eb01b8a99
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-12-20 20:25:03 +0900

    Feat : 메인페이지 모집 중인 모임 api 연결

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Header from '@/components/layout/Header/Header.tsx';
 import {
   Container,
@@ -10,43 +11,35 @@ import {
 import MyGroupsSection from '@/components/profile/MyPage/MyGroupsSection.tsx';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '@/constants/path.ts';
-import Card from '@/components/common/Card/Card';
-import { useCardData } from '@/hooks/mypage/useCardData';
 import { FindFriendRoomType } from '@/types/friend';
-
-const requireData: FindFriendRoomType[] = [
-  {
-    findFriendId: 3,
-    playgroundName: '서울 식물원 숲 문화학교 앞 놀이터',
-    title: '문화학교 앞 놀이터에서 놀사람 구해요',
-    description: '같이 놀 사람 구합니다',
-    scheduleTime: '2024. 12. 15 일요일 오후 5:00~오후 6:00',
-    recruitmentStatus: 'COMPLETE',
-    currentCount: 1,
-  },
-  {
-    findFriendId: 2,
-    playgroundName: '서울숲 유아숲 체험원',
-    title: '서울숲에서 놀사람 구합니다',
-    description: '구합니다 놀 사람',
-    scheduleTime: '2024. 12. 15 일요일 오후 10:08~오전 12:08',
-    recruitmentStatus: 'COMPLETE',
-    currentCount: 2,
-  },
-  {
-    findFriendId: 1,
-    playgroundName: '서리풀 상상나라 숲속학교 놀이터',
-    title: '바다육지 할 분~~~~~',
-    description: '오늘 학원 쨌습니다.',
-    scheduleTime: '2024. 12. 10 화요일 오후 5:30~오후 6:00',
-    recruitmentStatus: 'COMPLETE',
-    currentCount: 1,
-  },
-];
+import { getMainFindFriendList } from '@/api/findFriend/getMainFindFriendList';
 
 const MainPage = () => {
   const navigate = useNavigate();
-  const cardData = useCardData();
+  const [requireData, setRequireData] = useState<FindFriendRoomType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchFindFriendList = async () => {
+      try {
+        const data = await getMainFindFriendList();
+
+        const recruitingData = data.filter((item) => item.recruitmentStatus === 'RECRUITING');
+
+        setRequireData(recruitingData);
+      } catch (error) {
+        console.error('모집 중인 모임 데이터를 불러오는 중 오류 발생:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchFindFriendList();
+  }, []);
+
+  if (isLoading) {
+    return <div>로딩 중...</div>;
+  }
 
   return (
     <Container>
@@ -57,21 +50,13 @@ const MainPage = () => {
         <TitleText>모집 중인 모임</TitleText>
         <ViewMore onClick={() => navigate(PATH.MY_RECRUITMENTS('1'))}>더보기</ViewMore>
       </TitleContainer>
+
       <MyGroupsSection requireData={requireData} />
 
       <TitleContainer>
         <TitleText>새로운 친구</TitleText>
         <ViewMore onClick={() => navigate(PATH.FRIENDS_PLAYED('1'))}>더보기</ViewMore>
       </TitleContainer>
-      {cardData.map((item) => (
-        <Card
-          nickname={item.nickname}
-          status={item.roleType}
-          address={item.address}
-          image={item.image}
-          content={item.introduce}
-        />
-      ))}
     </Container>
   );
 };
