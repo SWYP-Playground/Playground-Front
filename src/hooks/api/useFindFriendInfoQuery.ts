6b75466866ade5a3523c9722a19ac02363e971cf
ncb6206 <ncb6206@gmail.com>
2024-12-21 01:24:55 +0900

    Refactor : 모집글 GET 관련 enabled 요소 추가

--- src/hooks/api/useFindFriendInfoQuery.ts
+++ src/hooks/api/useFindFriendInfoQuery.ts
@@ -1,13 +1,14 @@
 import { AxiosError } from 'axios';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 import { getFindFriendInfo } from '@/api/findFriend/getFindFriendInfo';
 import { FindFriendData } from '@/types/friend';
 
 export const useFindFriendInfoQuery = (findFriendId: number) => {
-  const { data: FindFriendInfoData } = useSuspenseQuery<FindFriendData, AxiosError>({
+  const { data: FindFriendInfoData } = useQuery<FindFriendData, AxiosError>({
     queryKey: ['findFriendInfo', findFriendId],
     queryFn: () => getFindFriendInfo(findFriendId),
+    enabled: !!findFriendId,
   });
 
   return { FindFriendInfoData };
