47780291d8a4924bcd6df4df5c041099ed7f3079
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-12-21 18:41:27 +0900

    Style : 최근 논 친구 배너 제거

--- src/pages/FriendsPlayedPage/FriendsPlayedPage.tsx
+++ src/pages/FriendsPlayedPage/FriendsPlayedPage.tsx
@@ -61,7 +61,6 @@ const FriendsPlayedPage = () => {
   return (
     <FriendsPlayedFlex>
       <Header title="최근 논 친구" leftIcon={<LeftIcon />} onLeftClick={goBackToPage} />
-      <Banner />
 
       {isLoading ? (
         <BlankText>데이터를 불러오는 중입니다...</BlankText>
